3cfd0c0f9f89c3e40bf623492ca1943f08ff0594
John Weir <john@smokinggun.com>
2016-03-07 10:21:50 -0800

    slightly nicer messaging

--- stopwatch.go
+++ stopwatch.go
@@ -21,12 +21,10 @@ type Timer struct {
 	Start time.Time
 }
 
-// commands
-//  start <name>
-//  list returns all running timers
-//  stop <name>
+// There is no file locking, so two processed running at the same time could cause a problem.
+// I might also win the lottery.
 func main() {
-	st, err := NewStopwatch()
+	st, err := newStopwatch()
 	if err != nil {
 		fmt.Printf("%s", err)
 		return
@@ -49,43 +47,12 @@ func main() {
 	st.start(label)
 }
 
-func (st *Stopwatch) start(label string) error {
-	t := Timer{label, time.Now()}
-	st.Timers = append(st.Timers, t)
-	st.write()
-
-	fmt.Printf("Started %s at %s\n", t.Label, t.Start)
-	return nil
+func (t Timer) toString() string {
+	d := time.Now().Sub(t.Start)
+	return fmt.Sprintf("'%s' running: %s started: %s\n", t.Label, d, t.Start)
 }
 
-func (st *Stopwatch) find(label string) (int, *Timer) {
-	for i, t := range st.Timers {
-		if t.Label == label {
-			return i, &t
-		}
-	}
-
-	return -1, nil
-}
-
-func (st *Stopwatch) stop(pos int) {
-	t := st.Timers[pos]
-	st.Timers = append(st.Timers[:pos], st.Timers[pos+1:]...)
-	st.write()
-	fmt.Printf("%s ran for %s (started at %s)\n", t.Label, time.Now().Sub(t.Start), t.Start)
-}
-
-func (st *Stopwatch) list() {
-	if len(st.Timers) == 0 {
-		fmt.Println("No stopwatches exist")
-	}
-
-	for _, t := range st.Timers {
-		fmt.Printf("%s at %s\n", t.Label, t.Start)
-	}
-}
-
-func NewStopwatch() (*Stopwatch, error) {
+func newStopwatch() (*Stopwatch, error) {
 	if _, err := os.Stat(filepath()); os.IsNotExist(err) {
 		return &Stopwatch{}, nil
 	}
@@ -104,6 +71,42 @@ func NewStopwatch() (*Stopwatch, error) {
 	}
 
 	return &st, err
+}
+
+func (st *Stopwatch) start(label string) error {
+	t := Timer{label, time.Now()}
+	st.Timers = append(st.Timers, t)
+	st.write()
+
+	fmt.Printf(t.toString())
+	return nil
+}
+
+func (st *Stopwatch) find(label string) (int, *Timer) {
+	for i, t := range st.Timers {
+		if t.Label == label {
+			return i, &t
+		}
+	}
+
+	return -1, nil
+}
+
+func (st *Stopwatch) stop(pos int) {
+	t := st.Timers[pos]
+	st.Timers = append(st.Timers[:pos], st.Timers[pos+1:]...)
+	st.write()
+	fmt.Printf("Stopped %s", t.toString())
+}
+
+func (st *Stopwatch) list() {
+	if len(st.Timers) == 0 {
+		fmt.Println("No stopwatches exist")
+	}
+
+	for _, t := range st.Timers {
+		fmt.Printf(t.toString())
+	}
 }
 
 func (st *Stopwatch) write() error {
@@ -129,7 +132,4 @@ func filepath() string {
 		log.Fatal(err)
 	}
 	return usr.HomeDir + "/.stopwatch.json"
-}
-
-func init() {
 }
